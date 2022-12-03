9564c53d5070907750d1908b3e81ca3f8114d51d
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-04 00:14:19 +0300

    Task 4

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿//task 4
+
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a >= b && a > c)
+{Console.WriteLine("Первое число больше других");
+
+}
+else if (b >= a && b >= c)
+{Console.WriteLine("Второе число больше других");  
+
+}
+else if (c >= a && c >= b)
+{Console.WriteLine("Третье число больше других");  
+
+}
